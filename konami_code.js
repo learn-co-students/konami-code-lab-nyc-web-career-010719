16ea74e37e7a5ab60363052ad6006417fe626784
github username <github email address>
2019-02-25 21:29:48 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,19 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
 
-function init() {
-  // your code here
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+function init(e) {
+  // Write your JavaScript code inside the init() function
+  let index = 0;
+  document.body.addEventListener('keydown', function (e){
+  const key = parseInt(e.detail || e.which);
+  if (key === code[index]) {
+    index++;
+    if (index === code.length) {
+      alert("Congratulation, the konami code has been submitted.");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+});
 }
